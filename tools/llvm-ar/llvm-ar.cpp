061658db2e0523f7326ad8bb9e2073bf011388e4
David Barksdale <amatus.amongus@gmail.com>
2013-05-13 22:44:38 -0500

    llvm-ar doesn't make directories
    For some reason the hasPath() test is failing on an archive created the libtool where each file starts with ".libs/". Seems safe enough to just always try to create directories. Not tested on any other archives.

--- tools/llvm-ar/llvm-ar.cpp
+++ tools/llvm-ar/llvm-ar.cpp
@@ -461,17 +461,15 @@ doExtract(std::string* ErrMsg) {
         (std::find(Paths.begin(), Paths.end(), I->getPath()) != Paths.end())) {
 
       // Make sure the intervening directories are created
-      if (I->hasPath()) {
-        sys::Path dirs(I->getPath());
-        dirs.eraseComponent();
-        if (dirs.createDirectoryOnDisk(/*create_parents=*/true, ErrMsg))
-          return true;
-      }
+      sys::Path dirs(I->getPath());
+      dirs.eraseComponent();
+      if (dirs.createDirectoryOnDisk(/*create_parents=*/true, ErrMsg))
+        return true;
 
       // Open up a file stream for writing
       std::ios::openmode io_mode = std::ios::out | std::ios::trunc |
                                    std::ios::binary;
-      std::ofstream file(I->getPath().c_str(), io_mode);
+      std::ofstream file(I->getPath().c_str(), io_mode); // TODO: Test that file is valid
 
       // Get the data and its length
       const char* data = reinterpret_cast<const char*>(I->getData());
